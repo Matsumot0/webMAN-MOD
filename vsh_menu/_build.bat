0dc48e497fe74f54816ad173213614c52137610f
Matsumot0 <oueshgrout@free.fr>
2015-10-12 22:49:01 +0200

    Updated build.bat of vsh menu for cygwin
    and remove useless cygz.dll in the root directory

--- vsh_menu/_build.bat
+++ vsh_menu/_build.bat
@@ -1,4 +1,9 @@
 @echo off
+set CYGWIN=C:\cygwin\bin
+
+if not exist %CYGWIN%\bash.exe set CYGWIN=C:\msys\1.0\bin
+set CHERE_INVOKING=1
+
 if exist wm_vsh_menu.sprx del /q wm_vsh_menu.sprx>nul
 if exist wm_vsh_menu.prx  del /q wm_vsh_menu.prx>nul
 if exist wm_vsh_menu.sym  del /q wm_vsh_menu.sym>nul
@@ -7,7 +12,7 @@ md data>nul
 if exist data\keys del /q data\*>nul
 copy ..\data\* data>nul
 
-make
+%CYGWIN%\bash --login -i -c 'make'
 
 del /q data\*>nul
 rd data>nul
